ba278a4818ee741069b54fbe8a7556d61647e636
Hans Jakob Bollinger <contact@webbear.ch>
2019-04-04 15:40:27 +0200

    safe javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-// maybe needed
+(function() {
+
+  if (document.querySelector(".grid-7x7")) {
+    var grid = document.querySelector(".grid-7x7 .grid");
+    var count = document.querySelector(".grid-7x7-container .number");
+
+    var counter = 0;
+    var limit = 49;
+
+    // build item
+    function item(c) {
+      var item = document.createElement("div");
+      item.innerHTML = "<span class='counter'>" + c + "</span>";
+      item.setAttribute("class", "grid-item");
+      return item;
+    }
+
+    // self-invoking, recursive function
+    (function appendItem() {
+      if (counter++ == limit) return;
+
+      setTimeout(function() {
+        console.log(counter);
+        grid.appendChild(item(counter));
+        count.innerHTML = counter;
+        // recursive
+        appendItem();
+      }, 100);
+    })();
+  }
+
+})();
