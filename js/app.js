1afa48ba1daad46950c7b16ab6a6bace35e3cbb0
Hans Jakob Bollinger <contact@webbear.ch>
2019-04-05 09:09:41 +0200

    polyfill ie11, set title by js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
+// ie 11 polyfill
+if (window.NodeList && !NodeList.prototype.forEach) {
+   console.info("forEach Polyfill für IE11");
+   NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+         callback.call(thisArg, this[i], i, this);
+      }
+   };
+}
+
 (function() {
 
   if (document.querySelector(".grid-7x7")) {
     var grid = document.querySelector(".grid-7x7 .grid");
     var count = document.querySelector(".grid-7x7-container .number");
 
+    var customItems = document.querySelectorAll(".custom-items .custom-item");
+    var customItemsCount =  customItems.length;
+
     var counter = 0;
     var limit = 49;
 
     // build item
-    function item(c) {
+    var item = function(c) {
       var item = document.createElement("div");
       item.innerHTML = "<span class='counter'>" + c + "</span>";
       item.setAttribute("class", "grid-item");
       return item;
-    }
+    };
 
     // self-invoking, recursive function
     (function appendItem() {
@@ -27,6 +41,24 @@
         appendItem();
       }, 100);
     })();
+
+    var makeTitle = function(item) {
+      var a = item.querySelector("a");
+      return a ? a.innerHTML = "<span class='title'>" + a.innerHTML + "</span>" : '';
+    };
+
+    if (customItems.length) {
+      customItems.forEach(makeTitle);
+
+      // for (var i = 0; i < customItemsCount; i++) {
+      //   var a = customItems[i].querySelector("a");
+      //   if (a) {
+      //     a.innerHTML = "<span class='title'>" + a.innerHTML + "</span>";
+      //   }
+      // }
+    }
+
+
   }
 
 })();
