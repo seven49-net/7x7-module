443da2f4e44dbf6f04463ef5ace80ce4e13d5927
Hans Jakob Bollinger <contact@webbear.ch>
2019-05-01 11:57:22 +0200

    add additional functionality

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
 // ie 11 polyfill
 if (window.NodeList && !NodeList.prototype.forEach) {
-   console.info("forEach Polyfill für IE11");
-   NodeList.prototype.forEach = function (callback, thisArg) {
-      thisArg = thisArg || window;
-      for (var i = 0; i < this.length; i++) {
-         callback.call(thisArg, this[i], i, this);
-      }
-   };
+  console.info("forEach Polyfill für IE11");
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
 }
 
-(function() {
+(function () {
 
   if (document.querySelector(".grid-7x7")) {
     var grid = document.querySelector(".grid-7x7 .grid");
     var count = document.querySelector(".grid-7x7-container .number");
 
     var customItems = document.querySelectorAll(".custom-items .custom-item");
-    var customItemsCount =  customItems.length;
+    var customItemsCount = customItems.length;
 
     var counter = 0;
     var limit = 49;
 
     // build item
-    var item = function(c) {
+    var item = function (c) {
       var item = document.createElement("div");
       item.innerHTML = "<span class='counter'>" + c + "</span>";
       item.setAttribute("class", "grid-item grid-item-" + c);
@@ -33,7 +33,7 @@ if (window.NodeList && !NodeList.prototype.forEach) {
     (function appendItem() {
       if (counter++ == limit) return;
 
-      setTimeout(function() {
+      setTimeout(function () {
         console.log(counter);
         grid.appendChild(item(counter));
         count.innerHTML = counter;
@@ -42,13 +42,20 @@ if (window.NodeList && !NodeList.prototype.forEach) {
       }, 50);
     })();
 
-    var makeTitle = function(item) {
-      var a = item.querySelector("a");
-      return a ? a.innerHTML = "<span class='title'>" + a.innerHTML + "</span>" : false;
+    var editContent = function (item) {
+      var classlist = item.classList;
+      if (classlist.contains("Link") || classlist.contains("Download")) {
+        var a = item.querySelector("a");
+        return a ? a.innerHTML = "<span class='title'>" + a.innerHTML + "</span>" : false;
+      }
+      if (classlist.contains("Text")) {
+        return item.innerHTML = "<div class='center-text'>" + item.innerHTML + "</div>";
+      }
+
     };
 
     if (customItems.length) {
-      customItems.forEach(makeTitle);
+      customItems.forEach(editContent);
 
       // for (var i = 0; i < customItemsCount; i++) {
       //   var a = customItems[i].querySelector("a");
